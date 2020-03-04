bebbb201d0d8e8801c9ccbcae97e7191b569b6f8
Pariwat Ramphuengnit <pariwat.ram@gmail.com>
2020-03-04 17:21:51 +0700

    Update myTests.test.js

--- __tests__/myTests.test.js
+++ __tests__/myTests.test.js
@@ -2,9 +2,14 @@ import wd from 'wd';
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 const PORT = 4723;
 const config = {
-    platformName: 'Android',
-    deviceName: 'Android Emulator',
-    app: '/Users/mhamed/demoAppium/android/app/build/outputs/apk/debug/app-debug.apk'
+    //platformName: 'Android',
+    //deviceName: 'Pixel_XL_API_28',
+    //app: '/Users/isnoopy/Documents/PTT_WS/PTT/PTT_DmService/appium_demo/android/app/build/outputs/apk/debug/app-debug.apk'
+
+    platformName: 'iOS',
+    platformVersion: '13.3',
+    deviceName: 'iPhone 11',
+    app: '/Users/isnoopy/Documents/PTT_WS/PTT/PTT_DmService/appium_demo/ios/build/demoAppium/Build/Products/Debug-iphonesimulator/demoAppium.app'
 };
 const driver = wd.promiseChainRemote('localhost', PORT);
 beforeAll(async () => {
